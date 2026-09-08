b10d803b11a7ca02e690325b71559f923f3a009d
Saulo <alfa@alfaautomacao.com.br>
2026-09-08 09:55:07 -0300

    Troca selo, icone e imagem de compartilhamento pela logo nova.

--- js/site.js
+++ js/site.js
@@ -419,7 +419,7 @@ const I18N = {
 
 const TOKEN_KEY = "wtc_token";
 const LANG_KEY = "wtc_lang";
-const MARK = "/img/badge.png?v=62";
+const MARK = "/img/badge.png?v=71";
 const WORD = "/img/wordmark-dark.png?v=62";
 
 function lang() {
